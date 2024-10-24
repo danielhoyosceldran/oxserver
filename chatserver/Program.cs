c131beff1cbdc265fde1954fbc8001806978adea
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2024-10-24 13:18:34 +0200

    code added

---
+++ chatserver/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.WebSockets;
+using System.Net;
+using System.Text;
+using System;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using chatserver.server;
+
+namespace chatserver
+{
+    class Program()
+    {
+        static async Task Main(string[] args)
+        {
+            // Més endevant m'iteressarà no tenir l'await
+            await Server.start();
+        }
+    }
+}
