53ed6948599d8cb263ebe7114c06b567f75addb4
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2024-11-04 16:58:35 +0100

    data base find functions done

--- chatserver/DDBB/DDBBHandler.cs
+++ chatserver/DDBB/DDBBHandler.cs
@@ -7,12 +7,14 @@ using MongoDB.Bson; // Per treballar amb documents BSON
 using MongoDB.Driver; // Per accedir a la funcionalitat de MongoDB
 using System.Text.Json;
 using System.Reflection.Metadata;
+using System.Security.Policy;
 
 namespace chatserver.DDBB
 {
     internal class DDBBHandler
     {
         MongoClient client;
+        private readonly string DATA_BASE_NAME = "chat";
         public static DDBBHandler Instance = new DDBBHandler();
         private DDBBHandler()
         {
@@ -25,13 +27,29 @@ namespace chatserver.DDBB
             Logger.DataBaseLogger.Debug("[write] - table: " + table);
             Logger.ConsoleLogger.Debug("[write] - table: " + table);
             // Accedir a la base de dades 'test'
-            var database = client.GetDatabase("chat");
+            var database = client.GetDatabase(DATA_BASE_NAME);
 
             // Accedir a la col·lecció 'testddbb'
             var collection = database.GetCollection<BsonDocument>(table);
             BsonDocument bsonDocument = BsonDocument.Parse(data.ToString());
 
             await collection.InsertOneAsync(bsonDocument);
+        }
+
+        public async Task<JsonDocument?> find(string table, string key, string value)
+        {
+            // Accedir a la base de dades 'test'
+            var database = client.GetDatabase(DATA_BASE_NAME);
+
+            // Accedir a la col·lecció 'testddbb'
+            var collection = database.GetCollection<BsonDocument>(table);
+
+            var filter = Builders<BsonDocument>.Filter.Eq(key, value); // Filtre per nom = "Joan"
+            BsonDocument result = await collection.Find(filter).FirstOrDefaultAsync();
+
+            return result == null 
+                ? null 
+                : JsonDocument.Parse(result.ToJson());
         }
     }
 }
